73d9fb73eb759b380d7151daba02a4b8714266b8
tamimmadisma <akonhjhjhj@gmail.com>
2026-09-04 20:12:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const m=document.querySelector('#menu'),n=document.querySelector('nav');m.onclick=()=>n.classList.toggle('open');document.querySelectorAll('nav a').forEach(a=>a.onclick=()=>n.classList.remove('open'));
+const m = document.querySelector('#menu');
+const n = document.querySelector('nav');
+
+m.onclick = () => n.classList.toggle('open');
+
+document.querySelectorAll('nav a').forEach(a => {
+  a.onclick = () => n.classList.remove('open');
+});
+
+
+/* START WEBSITE AT THE TOP */
+
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
